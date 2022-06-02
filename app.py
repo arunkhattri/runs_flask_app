8fcfc45929fcb30b5bee96d2cf732e1fdb30a975
Arun Kr. Khattri <arunkhattri@outlook.com>
2022-06-02 21:58:39 +0530

    added home and record pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Running Records</h1>"
+    return render_template("home.html")
+
+
+@app.route("/records")
+def personal_records():
+    return render_template("records.html")
 
 
 if __name__ == "__main__":
